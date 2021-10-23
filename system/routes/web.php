969db328ac851c7ec354e8188b65515c423d194b
Muhammad Khadafi <90817924+MuhammadKhadafi07@users.noreply.github.com>
2021-10-23 14:45:34 -0700

    Tugas 3

---
+++ system/routes/web.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('ecommerce.home');
+});
+
+Route::get('/about', function () {
+    return view('ecommerce.about');
+});
+
+Route::get('/checkout', function () {
+    return view('ecommerce.checkout');
+});
+
+Route::get('/contact', function () {
+    return view('ecommerce.contact');
+});
+
+Route::get('/login', function () {
+    return view('ecommerce.login');
+});
+
+Route::get('/register', function () {
+    return view('ecommerce.register');
+});
+
+Route::get('/sales', function () {
+    return view('ecommerce.sales');
+});
+
+Route::get('/single', function () {
+    return view('ecommerce.single');
+});
+
+// template admin
+Route::get('/beranda', function () {
+    return view('beranda');
+});
+
+Route::get('/produk', function () {
+    return view('produk');
+});
+
+Route::get('/kategori', function () {
+    return view('kategori');
+});
+
+Route::get('/user', function () {
+    return view('user');
+});
+
+Route::get('/adminlogin', function () {
+    return view('adminlogin');
+});
